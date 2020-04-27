17e404ff8456aebedc1aa0649d25a29afc81da05
vr3dina <vr3dina@gmail.com>
2020-04-27 21:05:29 +0300

    saving dialog

--- chatbot/src/main/java/chatbot/MainActivity.java
+++ chatbot/src/main/java/chatbot/MainActivity.java
@@ -1,6 +1,9 @@
 package chatbot;
 
+import android.content.ContentValues;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.speech.tts.TextToSpeech;
@@ -16,9 +19,12 @@ import androidx.appcompat.app.AppCompatDelegate;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Locale;
 
+import chatbot.db.DBHelper;
+import chatbot.db.MessageEntity;
 import chatbot.models.message.Message;
 import chatbot.models.message.MessageListAdapter;
 
@@ -29,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
     protected RecyclerView chatMessageList;
     protected TextToSpeech textToSpeech;
     protected MessageListAdapter messageListAdapter;
+
+    DBHelper dBHelper;
+    SQLiteDatabase database;
 
     SharedPreferences sPref;
     public static final String APP_PREFERENCES = "my_settings";
@@ -51,6 +60,35 @@ public class MainActivity extends AppCompatActivity {
         questionText = findViewById(R.id.questionField);
         chatMessageList = findViewById(R.id.chatMessageList);
 
+        messageListAdapter = new MessageListAdapter();
+        chatMessageList.setLayoutManager(new LinearLayoutManager(this));
+        chatMessageList.setAdapter(messageListAdapter);
+
+        dBHelper = new DBHelper(this);
+        database = dBHelper.getWritableDatabase();
+
+        Cursor cursor = database.query(DBHelper.TABLE_MESSAGES, null, null, null, null, null, null);
+        if (cursor.moveToFirst()){
+            int messageIndex = cursor.getColumnIndex(DBHelper.FIELD_MESSAGE);
+            int dateIndex = cursor.getColumnIndex(DBHelper.FIELD_DATE);
+            int sendIndex = cursor.getColumnIndex(DBHelper.FIELD_SEND);
+
+            do {
+                MessageEntity entity = new MessageEntity(
+                        cursor.getString(messageIndex),
+                        cursor.getString(dateIndex),
+                        cursor.getInt(sendIndex));
+                Message message = null;
+                try {
+                    message = new Message(entity);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                messageListAdapter.messageList.add(message);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+
         sendButton.setOnClickListener(view -> onSend());
 
         textToSpeech = new TextToSpeech(getApplicationContext(), i -> {
@@ -58,10 +96,13 @@ public class MainActivity extends AppCompatActivity {
                 textToSpeech.setLanguage(new Locale("ru"));
             }
         });
+    }
 
-        messageListAdapter = new MessageListAdapter();
-        chatMessageList.setLayoutManager(new LinearLayoutManager(this));
-        chatMessageList.setAdapter(messageListAdapter);
+    @Override
+    protected void onDestroy() {
+        database.close();
+        super.onDestroy();
+        Log.i("LOG", "onDestroy");
     }
 
     @Override
@@ -71,7 +112,20 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences.Editor editor = sPref.edit();
         editor.putBoolean(THEME, isLight);
         editor.apply();
+
+        database.delete(DBHelper.TABLE_MESSAGES, null, null);
+        for (Message message : messageListAdapter.messageList) {
+            MessageEntity entity = new MessageEntity(message);
+
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(DBHelper.FIELD_MESSAGE, entity.text);
+            contentValues.put(DBHelper.FIELD_SEND, entity.isSend);
+            contentValues.put(DBHelper.FIELD_DATE, entity.date);
+
+            database.insert(DBHelper.TABLE_MESSAGES,null,contentValues);
+        }
     }
+
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
@@ -102,15 +156,15 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putParcelableArrayList("list", (ArrayList<? extends Parcelable>) messageListAdapter.messageList);
         Log.i("LOG", "onSaveInstanceState");
         super.onSaveInstanceState(outState);
-        outState.putParcelableArrayList("list", (ArrayList<? extends Parcelable>) messageListAdapter.messageList);
     }
 
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
-        Log.i("LOG", "onRestoreInstanceState");
         super.onRestoreInstanceState(savedInstanceState);
+        Log.i("LOG", "onRestoreInstanceState");
         messageListAdapter.messageList = savedInstanceState.getParcelableArrayList("list");
         messageListAdapter.notifyDataSetChanged();
         chatMessageList.scrollToPosition(messageListAdapter.messageList.size() - 1);
