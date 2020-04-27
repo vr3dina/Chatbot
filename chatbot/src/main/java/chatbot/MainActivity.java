533f99de120ce63147225fa5f507b90bca36ace1
vr3dina <vr3dina@gmail.com>
2020-04-27 17:58:46 +0300

    saving theme mode

--- chatbot/src/main/java/chatbot/MainActivity.java
+++ chatbot/src/main/java/chatbot/MainActivity.java
@@ -1,8 +1,10 @@
 package chatbot;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
@@ -28,8 +30,20 @@ public class MainActivity extends AppCompatActivity {
     protected TextToSpeech textToSpeech;
     protected MessageListAdapter messageListAdapter;
 
+    SharedPreferences sPref;
+    public static final String APP_PREFERENCES = "my_settings";
+
+    private boolean isLight = true;
+    private String THEME = "THEME";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i("LOG", "onCreate");
+
+        sPref = getSharedPreferences(APP_PREFERENCES, MODE_PRIVATE);
+        isLight = sPref.getBoolean(THEME, true);
+        getDelegate().setLocalNightMode(isLight ? AppCompatDelegate.MODE_NIGHT_NO : AppCompatDelegate.MODE_NIGHT_YES);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -51,13 +65,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("LOG", "onStop");
+        SharedPreferences.Editor editor = sPref.edit();
+        editor.putBoolean(THEME, isLight);
+        editor.apply();
+    }
+
+    @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Log.i("LOG", "onOptionsItemSelected");
         switch (item.getItemId()) {
             case R.id.day_settings:
                 getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                isLight = true;
                 break;
             case R.id.night_settings:
                 getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                isLight = false;
                 break;
             default:
                 break;
@@ -68,6 +94,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        Log.i("LOG", "onCreateOptionsMenu");
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
     }
@@ -75,12 +102,14 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
+        Log.i("LOG", "onSaveInstanceState");
         super.onSaveInstanceState(outState);
         outState.putParcelableArrayList("list", (ArrayList<? extends Parcelable>) messageListAdapter.messageList);
     }
 
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        Log.i("LOG", "onRestoreInstanceState");
         super.onRestoreInstanceState(savedInstanceState);
         messageListAdapter.messageList = savedInstanceState.getParcelableArrayList("list");
         messageListAdapter.notifyDataSetChanged();
@@ -88,6 +117,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     protected void onSend() {
+        Log.i("LOG", "onSend");
         final String text = questionText.getText().toString();
         AI.getAnswer(text, answer -> {
             messageListAdapter.messageList.add(new Message(text, true));
